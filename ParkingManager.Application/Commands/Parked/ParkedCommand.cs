473e3f0d6cef3e4278f71f249f93cd9a21548ab0
Ewerton Ramos <elres.ewerton@gmail.com>
2026-07-12 16:13:29 -0300

    fix: Ajustando ParkedCommand.cs

--- ParkingManager.Application/Commands/Parked/ParkedCommand.cs
+++ ParkingManager.Application/Commands/Parked/ParkedCommand.cs
@@ -2,4 +2,4 @@ using ParkingManager.Domain.Enums;
 
 namespace ParkingManager.Application.Commands.Parked;
 
-public record ParkedCommand(string LicensePlate, DateTime ParkedTime, EventType EventType);
+public record ParkedCommand(string LicensePlate, string Latitude, string Longitude, EventType EventType);
