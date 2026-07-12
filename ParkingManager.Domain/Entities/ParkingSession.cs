ab561e43277004f5899c15f602ebc7c21ab09c13
Ewerton Ramos <elres.ewerton@gmail.com>
2026-07-12 00:42:48 -0300

    feat: Criando DbContext

--- ParkingManager.Domain/Entities/ParkingSession.cs
+++ ParkingManager.Domain/Entities/ParkingSession.cs
@@ -1,13 +1,31 @@
+using ParkingManager.Domain.Enums;
+
 namespace ParkingManager.Domain.Entities;
 
 public class ParkingSession
 {
     public string LicensePlate { get; private set; }
     public DateTime EntryTime { get; private set; }
+    public DateTime? ExitTime { get; private set; }
+    public SessionStatus Status { get; private set; }
+    public decimal? TotalAmount { get; private set; }
     
     public ParkingSession(string licensePlate, DateTime entryTime)
     {
         LicensePlate = licensePlate;
         EntryTime = entryTime;
+        Status = SessionStatus.Active;
+    }
+
+    public void Finish(DateTime exitTime, decimal totalAmount)
+    {
+        if (Status == SessionStatus.Finished)
+        {
+            throw new InvalidOperationException("Session is already finished.");
+        }
+
+        ExitTime = exitTime;
+        TotalAmount = totalAmount;
+        Status = SessionStatus.Finished;
     }
 }
