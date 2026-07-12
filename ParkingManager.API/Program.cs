9ca91051f1940f79ba3df77ee90fe8a4abf57b7a
Ewerton Ramos <elres.ewerton@gmail.com>
2026-07-12 16:13:03 -0300

    build: Adicionando Handles e a contrller ao program.cs

--- ParkingManager.API/Program.cs
+++ ParkingManager.API/Program.cs
@@ -1,4 +1,14 @@
+using ParkingManager.Application.Commands.Entry;
+using ParkingManager.Application.Commands.Exit;
+using ParkingManager.Application.Commands.Parked;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<EntryCommandHandler>();
+builder.Services.AddScoped<ParkedCommandHandler>();
+builder.Services.AddScoped<ExitCommandHandler>();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,30 +25,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.MapControllers();
