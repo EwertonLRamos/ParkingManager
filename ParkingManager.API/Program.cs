4bcdfd6d725d5da5c237dc6ecda90fb070567862
Ewerton Ramos <elres.ewerton@gmail.com>
2026-07-13 01:47:52 -0300

    feat: Adicionando conexão com a base de dados

--- ParkingManager.API/Program.cs
+++ ParkingManager.API/Program.cs
@@ -1,11 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using ParkingManager.Application.Commands.Entry;
 using ParkingManager.Application.DependencyInjection.Extensions;
+using ParkingManager.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddCommandHandlers(typeof(EntryCommandHandler).Assembly);
+
+builder.Services.AddDbContext<ParkingManagerDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
