76747c01a0f0bb4c3a5653c418a28e73e156d9f2
Ewerton Ramos <elres.ewerton@gmail.com>
2026-07-12 23:52:20 -0300

    fix: Implementando DBContext e mapeando entidades

--- ParkingManager.Infrastructure/Data/ParkingManagerDbContext.cs
+++ ParkingManager.Infrastructure/Data/ParkingManagerDbContext.cs
@@ -3,14 +3,105 @@ using ParkingManager.Domain.Entities;
 
 namespace ParkingManager.Infrastructure.Data;
 
-public class ParkingManagerDbContext : DbContext
+public class ParkingManagerDbContext(DbContextOptions<ParkingManagerDbContext> options) : DbContext(options)
 {
-    public ParkingManagerDbContext(DbContextOptions<ParkingManagerDbContext> options) : base(options)
-    {
-    }
-
     public DbSet<Sector> Sectors { get; set; }
     public DbSet<Spot> Spots { get; set; }
-    public DbSet<Vehicle> Vehicles { get; set; }
     public DbSet<ParkingSession> ParkingSessions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Sector>(entity =>
+        {
+            entity.ToTable("Sectors");
+
+
+            entity.HasKey(s => s.Id);
+
+            entity.Property(s => s.Name)
+                .IsRequired()
+                .HasMaxLength(5);
+
+            entity.Property(s => s.BasePrice)
+                .HasPrecision(10, 2)
+                .IsRequired();
+
+            entity.Property(s => s.MaxCapacity)
+                .IsRequired();
+
+            entity.HasMany(s => s.Spots)
+                .WithOne(s => s.Sector)
+                .HasForeignKey(s => s.SectorId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+            entity.HasIndex(s => s.Name);
+        });
+
+        modelBuilder.Entity<Spot>(entity =>
+        {
+            entity.ToTable("Spots");
+
+
+            entity.HasKey(s => s.Id);
+
+            entity.Property(s => s.Latitude)
+                .HasPrecision(9, 6)
+                .IsRequired();
+
+            entity.Property(s => s.Longitude)
+                .HasPrecision(9, 6)
+                .IsRequired();
+
+            entity.Property(s => s.SectorId)
+                .IsRequired();
+
+
+            entity.HasIndex(s => new { s.Latitude, s.Longitude });
+        });
+
+        modelBuilder.Entity<ParkingSession>(entity =>
+        {
+            entity.ToTable("ParkingSessions");
+
+
+            entity.HasKey(ps => ps.Id);
+
+            entity.Property(ps => ps.LicensePlate)
+                .IsRequired()
+                .HasMaxLength(7);
+
+            entity.Property(ps => ps.EntryTime)
+                .IsRequired();
+
+            entity.Property(ps => ps.ExitTime)
+                .IsRequired(false);
+
+            entity.Property(ps => ps.OccupancyRate)
+                .IsRequired();
+
+            entity.Property(ps => ps.Status)
+                .IsRequired()
+                .HasConversion<string>();
+
+            entity.Property(ps => ps.SetorName)
+                .HasMaxLength(5)
+                .IsRequired(false);
+
+            entity.Property(ps => ps.TotalAmount)
+                .HasPrecision(10, 2)
+                .IsRequired(false);
+
+
+            entity.HasIndex(ps => ps.LicensePlate);
+
+            entity.HasIndex(ps => ps.Status);
+
+            entity.HasIndex(ps => ps.EntryTime);
+
+            entity.HasIndex(ps => new { ps.SetorName, ps.EntryTime });
+        });
+    }
 }
