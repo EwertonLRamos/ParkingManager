5697592e31edb34be89dabb6ca80b39728dd7b05
Ewerton Ramos <elres.ewerton@gmail.com>
2026-07-14 00:26:19 -0300

    feat: Implementando lógica do handler de entry

--- ParkingManager.Application/Commands/Entry/EntryCommandHandler.cs
+++ ParkingManager.Application/Commands/Entry/EntryCommandHandler.cs
@@ -1,12 +1,31 @@
 using ParkingManager.Application.DependencyInjection.Interfaces;
+using ParkingManager.Domain.Entities;
+using ParkingManager.Domain.Repositories;
+using ParkingManager.Domain.Services;
 
 namespace ParkingManager.Application.Commands.Entry;
 
-public class EntryCommandHandler : ICommandHandler<EntryCommand>
+public class EntryCommandHandler(
+    IParkingSessionRepository parkingSessionRepository,
+    ISectorRepository sectorRepository,
+    ISpotRepository spotRepository) : ICommandHandler<EntryCommand>
 {
-    public Task HandleAsync(EntryCommand command)
+    private readonly IParkingSessionRepository _parkingSessionRepository = parkingSessionRepository;
+    private readonly ISectorRepository _sectorRepository = sectorRepository;
+    private readonly ISpotRepository _spotRepository = spotRepository;
+
+    public async Task HandleAsync(EntryCommand command)
     {
-        // Implementation for handling entry command
-        return Task.CompletedTask;
+        var occupiedSpots = await _spotRepository.GetTotalOccupiedSpotsAsync();
+        var totalSpots = await _sectorRepository.GetTotalSpotsAsync();
+
+        var occupancyRate = OccupancyRateService.CalculateOccupancyRate(occupiedSpots, totalSpots);
+
+        if(occupancyRate >= 1.0)
+            throw new InvalidOperationException("The parking lot is at 100% capacity.");
+
+        var parkingSession = new ParkingSession(command.LicensePlate, command.EntryTime, occupancyRate);
+
+        await _parkingSessionRepository.AddAsync(parkingSession);
     }
 }
