403868a5a830ad3f8efcd91c17df57fcd4f543b9
Ewerton Ramos <elres.ewerton@gmail.com>
2026-07-14 02:26:46 -0300

    feat: Implementando fluxo do handler do evento de parked

--- ParkingManager.Application/Commands/Parked/ParkedCommand.cs
+++ ParkingManager.Application/Commands/Parked/ParkedCommand.cs
@@ -3,4 +3,4 @@ using ParkingManager.Domain.Enums;
 
 namespace ParkingManager.Application.Commands.Parked;
 
-public record ParkedCommand(string LicensePlate, string Latitude, string Longitude, EventType EventType) : ICommand;
+public record ParkedCommand(string LicensePlate, decimal Latitude, decimal Longitude, EventType EventType) : ICommand;
--- ParkingManager.Application/Commands/Parked/ParkedCommandHandler.cs
+++ ParkingManager.Application/Commands/Parked/ParkedCommandHandler.cs
@@ -1,12 +1,30 @@
 using ParkingManager.Application.DependencyInjection.Interfaces;
+using ParkingManager.Domain.Repositories;
 
 namespace ParkingManager.Application.Commands.Parked;
 
-public class ParkedCommandHandler : ICommandHandler<ParkedCommand>
+public class ParkedCommandHandler(IParkingSessionRepository parkingSessionRepository, ISpotRepository spotRepository) : ICommandHandler<ParkedCommand>
 {
-    public Task HandleAsync(ParkedCommand command)
+    private readonly IParkingSessionRepository _parkingSessionRepository = parkingSessionRepository;
+    private readonly ISpotRepository _spotRepository = spotRepository;
+
+    public async Task HandleAsync(ParkedCommand command)
     {
-        // Implementation for handling parked command
-        return Task.CompletedTask;
+        var spot = await _spotRepository.GetByCoordinatesAsync(command.Latitude, command.Longitude) ?? 
+            throw new InvalidOperationException($"No spot found with coordinates: Lat. {command.Latitude} and Long. {command.Longitude}.");
+
+        if(spot.IsOccupied)
+            throw new InvalidOperationException($"The spot at coordinates: Lat. {command.Latitude} and Long. {command.Longitude} is already occupied.");
+
+        var parkingSession = await _parkingSessionRepository.GetActiveSessionByLicensePlateAsync(command.LicensePlate) ?? 
+            throw new InvalidOperationException($"No active parking session found for license plate {command.LicensePlate}.");
+
+        spot.Occupy();
+
+        await _spotRepository.UpdateAsync(spot);
+
+        parkingSession.Park(command.Latitude, command.Longitude);
+
+        await _parkingSessionRepository.UpdateAsync(parkingSession);
     }
 }
