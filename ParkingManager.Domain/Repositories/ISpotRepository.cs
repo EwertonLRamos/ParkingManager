c439fade09f2fb5649b599120a845091867cf8f2
Ewerton Ramos <elres.ewerton@gmail.com>
2026-07-14 02:12:50 -0300

    feat: Adicionando ações básicas de adição e atualização ao repositório de spot

--- ParkingManager.Domain/Repositories/ISpotRepository.cs
+++ ParkingManager.Domain/Repositories/ISpotRepository.cs
@@ -4,6 +4,8 @@ namespace ParkingManager.Domain.Repositories;
 
 public interface ISpotRepository
 {
+    Task AddAsync(Spot spot);
+    Task UpdateAsync(Spot spot);
     Task<int> GetTotalOccupiedSpotsAsync();
     Task<Spot?> GetByCoordinatesAsync(decimal latitude, decimal longitude);
 }
--- ParkingManager.Infrastructure/Repositories/SpotRepository.cs
+++ ParkingManager.Infrastructure/Repositories/SpotRepository.cs
@@ -17,4 +17,16 @@ public class SpotRepository(ParkingManagerDbContext context) : ISpotRepository
                 s.Latitude == latitude && 
                 s.Longitude == longitude
             );
+
+    public async Task AddAsync(Spot spot)
+    {
+        await _context.Spots.AddAsync(spot);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Spot spot)
+    {
+        _context.Spots.Update(spot);
+        await _context.SaveChangesAsync();
+    }
 }
