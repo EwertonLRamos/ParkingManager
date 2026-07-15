f17411ab66021dbebaa95f79035343fcbeb7befd
Ewerton Ramos <elres.ewerton@gmail.com>
2026-07-15 17:10:22 -0300

    feat: Criando controlador de revenue

---
+++ ParkingManager.API/Controllers.cs/RevenueController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using ParkingManager.Application.DependencyInjection.Dispatchers;
+using ParkingManager.Application.Queries.Revenue;
+
+namespace ParkingManager.API.Controllers;
+
+public class RevenueController(CommandDispatcher commandDispatcher) : ControllerBase
+{
+    private readonly CommandDispatcher _commandDispatcher = commandDispatcher;
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetRevenue([FromQuery] string sector, [FromQuery] DateOnly date)
+    {
+        var revenueQuery = new RevenueQuery(date, sector);
+
+        var revenueResponse = await _commandDispatcher.DispatchAsync<RevenueQuery, RevenueQueryResponse>(revenueQuery);
+
+        return Ok(revenueResponse);
+    }
+}
