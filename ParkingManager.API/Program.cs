ef648175dec5db6aa064fabe401da2aa23a494d1
Ewerton Ramos <elres.ewerton@gmail.com>
2026-07-16 23:14:10 -0300

    cleanup: Removendo comentários do Program.cs

--- ParkingManager.API/Program.cs
+++ ParkingManager.API/Program.cs
@@ -30,15 +30,12 @@ builder.Services.AddScoped<IParkingSessionRepository, ParkingSessionRepository>(
 builder.Services.AddScoped<ISectorRepository, SectorRepository>();
 builder.Services.AddScoped<ISpotRepository, SpotRepository>();
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
